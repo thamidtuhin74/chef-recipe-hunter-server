d05d7ff1fc5f53495e76b5784628573f1e946053
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-05-02 03:01:13 +0600

    create api link for local servers

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World! Chef Server is running')
 })
 
+app.get('/chefs',(req, res)=>{
+    res.send(chefs);//to display in screen
+})
+
+//display loaded data with dynamic link
+app.get('/chefs/:id',(req, res)=>{
+    //here we create  dynamic server link like : http://localhost:3000/chefs/4
+const id = parseInt(req.params.id);
+console.log('lets see  the chefs id ;' + id );
+const chef  = chefs.find(chef => chef.id === id) || {}
+res.send(chef);// dispay in screen
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
